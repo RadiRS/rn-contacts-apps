f2ab0998dd7fcd8af9cac3fc41cb8e5f9a238565
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-19 14:57:28 +0700

    Reconfig authentication flow

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,5 +1,7 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
+import {ActivityIndicator} from 'react-native';
 
 import AuthNavigator from './auth-navigator';
 import DrawerNavigator from './drawer-navigator';
@@ -7,14 +9,44 @@ import DrawerNavigator from './drawer-navigator';
 import {GlobalContext} from '../context/provider';
 
 const AppNavContainer = () => {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [isAuthLoaded, setAuthLoaded] = useState(false);
   const {authState} = useContext(GlobalContext);
 
-  const isLoggedIn = authState.isLoggedIn;
+  const getUser = async () => {
+    try {
+      const user = await AsyncStorage.getItem('user');
+
+      if (user) {
+        setAuthLoaded(true);
+        setAuthenticated(true);
+      } else {
+        setAuthLoaded(true);
+        setAuthenticated(false);
+      }
+    } catch (error) {
+      setAuthLoaded(true);
+      console.warn(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <>
+      {!isAuthLoaded && <ActivityIndicator />}
+      {isAuthLoaded && (
+        <NavigationContainer>
+          {authState.isLoggedIn || isAuthenticated ? (
+            <DrawerNavigator />
+          ) : (
+            <AuthNavigator />
+          )}
+        </NavigationContainer>
+      )}
+    </>
   );
 };
 
